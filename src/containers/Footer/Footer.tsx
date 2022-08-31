eabbf6986c20eba77fd18f9ea417a9d56c0d3152
daonghia2404 <daonghia2404@gmail.com>
2022-08-31 13:27:37 +0700

    Fix bugs feedback

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -81,7 +81,7 @@ const Footer: React.FC<TFooterProps> = () => {
                   expandIconPosition="right"
                   accordion
                   expandIcon={({ isActive }): React.ReactElement => (
-                    <div className="Faq-panel-icon">
+                    <div className="Footer-panel-icon">
                       {isActive ? (
                         <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
@@ -142,7 +142,7 @@ const Footer: React.FC<TFooterProps> = () => {
                   expandIconPosition="right"
                   accordion
                   expandIcon={({ isActive }): React.ReactElement => (
-                    <div className="Faq-panel-icon">
+                    <div className="Footer-panel-icon">
                       {isActive ? (
                         <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
